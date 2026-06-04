d0ff164367cdcb3b832aa2adc40be820b4565e29
Anastasija Kipcevic <akiptsevich@gmail.com>
2026-06-04 11:59:07 +0200

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://beltransoil-service.by",
+  site: "https://qkipa00.github.io",
+  base: "/beltransoil-service",
   devToolbar: {
     enabled: false,
   },
